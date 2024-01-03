110b16e382af88384778e69f1df65e33eb349cd6
s-rmeng <s-rmeng@487M124710.lwsd.k12wa.org>
2024-01-03 08:14:36 -0800

    1/3 Commit

--- TravelingSalesman/src/Tour.java
+++ TravelingSalesman/src/Tour.java
@@ -4,7 +4,7 @@
  * Mrs. Kankelborg
  * Period 1
  * Project 1 Traveling Salesman
- * Last Revised on: 12/14/2023 
+ * Last Revised on: 1/2/2024 
  * </pre>
  */
 public class Tour
@@ -29,13 +29,31 @@ public class Tour
      */
     public Tour()
     {
+    	home = null;
+    	size = 0;
     }
 
     /**
      * Creates the 4-point tour a->b->c->d->a (for debugging)
      */
-    public Tour(Point a, Point b, Point c, Pointd)
+    public Tour(Point a, Point b, Point c, Point d)
     {
+    	home.data = a;
+    	home.next.next = new Node();
+    	
+    	home.next.data = b;
+    	home.next.next.next = new Node();
+    	
+    	home.next.next.data = c;
+    	home.next.next.next.next = new Node();
+    	
+    	home.next.next.next.data = d;
+    	home.next.next.next.next = new Node();
+    	
+    	home.next.next.next.next = home;
+    	home.next.next = new Node();
+    	
+    	size = 4;
     }
     
     /**
@@ -43,7 +61,7 @@ public class Tour
      */
     public int size()
     {
-        return 0;
+        return size;
     }
 
     /**
@@ -51,16 +69,37 @@ public class Tour
      */
     public double length()
     {
-        return 0;
+ 	    double totalLength = 0;
+        Node current = home;
+        
+        while (current != null && current.next != home) {
+            totalLength += current.data.distanceTo(current.next.data);
+            current = current.next;
+        }
+        if (current != null) {
+            totalLength += current.data.distanceTo(home.data);
+        }
+        
+        return totalLength;
     }
 
     /**
      * Returns a string representation of this tour.
      */
-    @Override
     public String toString()
     {
-        return null;
+        StringBuilder sb = new StringBuilder();
+        Node current = home;
+        while (current != null && current.next != home) {
+            sb.append(current.data.toString()).append(" -> ");
+            current = current.next;
+        }
+        if (current != null) {
+            sb.append(current.data.toString()).append(" -> ");
+            sb.append(home.data.toString()); // Closing the loop
+        }
+        return sb.toString();
+    
     }
 
     /**
@@ -68,6 +107,14 @@ public class Tour
      */
     public void draw()
     {
+    	Node current = home;
+        while (current != null && current.next != home) {
+            current.data.drawTo(current.next.data);
+            current = current.next;
+        }
+        if (current != null) {
+            current.data.drawTo(home.data); // Closing the loop
+        }
     }
 
     /**
@@ -75,6 +122,7 @@ public class Tour
      */
     public void insertNearest(Point p)
     {
+    	
     }
 
     /**
